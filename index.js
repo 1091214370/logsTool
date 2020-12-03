8eb34143d8482d31a645754829c15712cc7eeeb6
1091214370@qq.com <1091214370@qq.com>
2020-12-03 21:38:58 +0800

    commit信息获取逻辑调整

--- index.js
+++ index.js
@@ -8,17 +8,9 @@ program
   .option('-d, --days', '查看[days]天之内的提交记录')
   .option('-c, --commit', '填写git commit -m 的内容')
   .parse(process.argv);
-
-  let message;
-  if (process.argv.length > 4) {
-    const messageArr = process.argv.slice(3);
-    message = messageArr.join(' ');
-  } else {
-    message = process.argv[3];
-  }
 // 打印git 提交日志
 if (program.days) logs(process.argv[3]);
 
 // git 便捷提交
-if (program.commit) commit(message)
+if (program.commit) commit(process.argv[3])
 
